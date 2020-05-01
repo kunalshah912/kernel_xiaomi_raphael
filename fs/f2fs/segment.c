56200099bff7eb63a24da3ab86463e740a2daaf5
UtsavBalar1231 <utsavbalar1231@gmail.com>
2020-05-01 10:19:53 +0530

    Revert "Merge remote-tracking branch 'aosp/upstream-f2fs-stable-linux-4.14.y' into android-4.14-stable"
    This reverts commit 355d2b65ceec47ac7df5eccce16a43ccf1377267, reversing
    changes made to 4d701a3899580b291122ab7b147bc20981afd349.

--- fs/f2fs/segment.c
+++ fs/f2fs/segment.c
@@ -1029,9 +1029,9 @@ static void f2fs_submit_discard_endio(struct bio *bio)
 	struct discard_cmd *dc = (struct discard_cmd *)bio->bi_private;
 	unsigned long flags;
 
+	dc->error = blk_status_to_errno(bio->bi_status);
+
 	spin_lock_irqsave(&dc->lock, flags);
-	if (!dc->error)
-		dc->error = blk_status_to_errno(bio->bi_status);
 	dc->bio_ref--;
 	if (!dc->bio_ref && dc->state == D_SUBMIT) {
 		dc->state = D_DONE;
@@ -1101,6 +1101,7 @@ static void __init_discard_policy(struct f2fs_sb_info *sbi,
 	} else if (discard_type == DPOLICY_FSTRIM) {
 		dpolicy->io_aware = false;
 	} else if (discard_type == DPOLICY_UMOUNT) {
+		dpolicy->max_requests = UINT_MAX;
 		dpolicy->io_aware = false;
 		/* we need to issue all to keep CP_TRIMMED_FLAG */
 		dpolicy->granularity = 1;
@@ -1214,10 +1215,8 @@ submit:
 		len = total_len;
 	}
 
-	if (!err && len) {
-		dcc->undiscard_blks -= len;
+	if (!err && len)
 		__update_discard_tree_range(sbi, bdev, lstart, start, len);
-	}
 	return err;
 }
 
@@ -1464,8 +1463,6 @@ next:
 
 	return issued;
 }
-static unsigned int __wait_all_discard_cmd(struct f2fs_sb_info *sbi,
-					struct discard_policy *dpolicy);
 
 static int __issue_discard_cmd(struct f2fs_sb_info *sbi,
 					struct discard_policy *dpolicy)
@@ -1474,14 +1471,12 @@ static int __issue_discard_cmd(struct f2fs_sb_info *sbi,
 	struct list_head *pend_list;
 	struct discard_cmd *dc, *tmp;
 	struct blk_plug plug;
-	int i, issued;
+	int i, issued = 0;
 	bool io_interrupted = false;
 
 	if (dpolicy->timeout)
 		f2fs_update_time(sbi, UMOUNT_DISCARD_TIMEOUT);
 
-retry:
-	issued = 0;
 	for (i = MAX_PLIST_NUM - 1; i >= 0; i--) {
 		if (dpolicy->timeout &&
 				f2fs_time_over(sbi, UMOUNT_DISCARD_TIMEOUT))
@@ -1526,11 +1521,6 @@ next:
 
 		if (issued >= dpolicy->max_requests || io_interrupted)
 			break;
-	}
-
-	if (dpolicy->type == DPOLICY_UMOUNT && issued) {
-		__wait_all_discard_cmd(sbi, dpolicy);
-		goto retry;
 	}
 
 	if (!issued && io_interrupted)
